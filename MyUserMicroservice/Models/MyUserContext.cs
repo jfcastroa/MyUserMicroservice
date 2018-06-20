b6b8e842fac594ae5957f94b787b94e2a6cffa38
Juan Fernando Castro <juanfercas2002@gmail.com>
2018-06-20 09:01:50 -0500

    Agregar archivos de proyecto.

---
+++ MyUserMicroservice/Models/MyUserContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyUserMicroservice.Models
+{
+    public partial class MyUserContext : DbContext
+    {
+        public MyUserContext()
+        {
+        }
+
+        public MyUserContext(DbContextOptions<MyUserContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=JUANFDOCASTRO\\SQLEXPRESS;Database=MyUser;Trusted_Connection=True;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Ciudad).HasMaxLength(50);
+
+                entity.Property(e => e.Direccion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(320)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoTel)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Pais).HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ MyUserMicroservice/UserContextSeed.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using MyUserMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUserMicroservice
+{
+    public class UserContextSeed
+    {
+
+       
+
+       
+
+        public static async Task SeedAsync(MyUserContext contextuser)
+        {
+
+
+            var context = contextuser;
+
+
+
+
+            using (context)
+            {
+                context.Database.Migrate();
+                if (!context.User.Any())
+                {
+                    context.User.AddRange(
+                        GetPreconfiguredUsers());
+                    await context.SaveChangesAsync();
+                }
+               
+            }
+        }
+
+        static IEnumerable<User> GetPreconfiguredUsers()
+        {
+            return new List<User>()
+       {
+           new User() { Nombre="Usuario 1", Email="Email 1", Ciudad="Ciudad 1", Direccion="Direccion 1", NoTel="Tel 1", Pais="Pais 1", Password=MD5Hash("Password 1"), Verificado=true},
+           new User() { Nombre="Usuario 2", Email="Email 2", Ciudad="Ciudad 2", Direccion="Direccion 2", NoTel="Tel 2", Pais="Pais 2", Password=MD5Hash("Password 2"), Verificado=false },
+           new User() { Nombre="Usuario 3", Email="Email 3", Ciudad="Ciudad 3", Direccion="Direccion 3", NoTel="Tel 3", Pais="Pais 3", Password=MD5Hash("Password 3"), Verificado=true },
+           new User() { Nombre="Usuario 4", Email="Email 4", Ciudad="Ciudad 4", Direccion="Direccion 4", NoTel="Tel 4", Pais="Pais 4", Password=MD5Hash("Password 4"), Verificado=false }
+       };
+        }
+
+        public static string MD5Hash(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(input));
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+
+
+    }
+}
